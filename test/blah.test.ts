f2b3b6c44fefda111504a75f2dde5a929b377eac
Felix Green <work@felix.green>
2020-03-05 15:30:20 -0500

    🚀 initial commit

---
+++ test/blah.test.ts
@@ -0,0 +1,9 @@
+// TODO: what do tests even look like?
+
+// import { sum } from '../src';
+
+// describe('blah', () => {
+//   it('works', () => {
+//     expect(sum(1, 1)).toEqual(2);
+//   });
+// });
